765bce08df358fd0151b2e39b958ad4dd617cdb1
hotline <32012110+hotline1337@users.noreply.github.com>
2022-03-14 19:21:53 +0100

    Create anti_afk.lua

---
+++ anti_afk.lua
@@ -0,0 +1,7 @@
+callbacks.register("post_move", function(cmd)
+    if not client.is_alive() then return end
+
+    if cmd.command_number % 2 == 0 then
+        cmd.buttons = bit.bor(cmd.buttons, 2^27)
+    end
+end)
